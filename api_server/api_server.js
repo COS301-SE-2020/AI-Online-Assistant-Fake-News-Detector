bf2135533a0db4e627a87529ae3221126bf97375
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-06-10 02:58:52 +0200

    Added base routes for the api calls. API server interacts with the db server. Created a basic front end 3 page system. Has zero information.

--- api_server/api_server.js
+++ api_server/api_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 const bodyParser = require("body-parser");
 const path = require("path");
-const port = 3000;
+const port = 8080;
 var cors = require("cors");
 // Root path of server
 const root = require("./Util/path");
@@ -10,18 +10,30 @@ const root = require("./Util/path");
 const APIRoutes = require("./routes/api");
 
 //Static files like css or js frontend.
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static(path.join(root, "Content")));
+server.use(express.static(path.join(root, "/node_modules/bootstrap/dist")));
+server.use(express.static(path.join(root, "Content")));
 server.use(express.static(path.join(root, "Content")));
 
 server.use("/API", APIRoutes);
 
 server.get("/", (req, res, next) => {
-  res.status(200).sendFile(path.join(root, "views", "index.html"));
+  res.status(200).sendFile(path.join(root, "Views", "index.html"));
+});
+
+server.get("/ProjectGuide", (req, res, next) => {
+  res.status(200).sendFile(path.join(root, "Views", "ProjectGuide.html"));
+});
+
+server.get("/TheTeam", (req, res, next) => {
+  res.status(200).sendFile(path.join(root, "Views", "TheTeam.html"));
 });
 
 server.use((req, res, next) => {
   res.status(404).sendFile(path.join(root, "views", "404.html"));
 });
 
-let listener = server.listen(8080, () => {
+let listener = server.listen(port, () => {
   console.log("Server listening on port " + listener.address().port);
 });
--- api_server/routes/api.js
+++ api_server/routes/api.js
@@ -1,12 +1,122 @@
 const express = require("express");
 const api = express.Router();
+const http = require("http");
 
+/**
+ * @description base get request route. Reroutes teh api call to the api server
+ * @author Stuart Barclay
+ */
 api.get("/", (req, res) => {
   res.status(200);
   res.json({ Response: "Hello from the API" });
 });
 
-//Requests that passed the routes above are not supported and therefore seen as errors.
+/**
+ * @description base get request route. Reroutes the api call to the api server, gets all sources in the db
+ * @author Stuart Barclay
+ */
+
+api.get("/sources", (req, res, next) => {
+  let request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources",
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf8");
+      response.on("data", (chunk) => {
+        res.status(response.statusCode).send(chunk);
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.end();
+});
+
+/**
+ * @description base post request route. Reroutes the api call to the api server. adds new source to db.
+ * @author Stuart Barclay
+ */
+
+api.post("/sources", (req, res, next) => {
+  let requestBody = "";
+  try {
+    requestBody = JSON.stringify(req.body);
+  } catch (e) {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  }
+  let request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(requestBody),
+      },
+    },
+    (response) => {
+      response.setEncoding("utf8");
+      response.on("data", (chunk) => {
+        res.status(response.statusCode).send(chunk);
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.write(requestBody);
+  request.end();
+});
+
+/**
+ * @description base get request route. Reroutes the api call to the api server, gets all sources in the db
+ * @author Stuart Barclay
+ */
+
+api.get("/sources/:sourceId", (req, res, next) => {
+  let request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources/" + req.params.sourceId,
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf8");
+      response.on("data", (chunk) => {
+        res.status(response.statusCode).send(chunk);
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+/**
+ * @description API calls done incorrectly will arrive at this route and be dismissed.
+ * @author Stuart Barclay
+ */
+
 api.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
