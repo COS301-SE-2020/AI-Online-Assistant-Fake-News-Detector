ebe80e7c2567c7c2d326176755d7178a544e76c0
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-07-11 12:19:47 +0200

    Small changes to the paths

---
+++ node_modules/api_server/routes/APIv1.js
@@ -0,0 +1,552 @@
+const api = require("express").Router();
+const http = require("http");
+
+/**
+ * @description base get request route. Reroutes teh api call to the api server
+ * @author Stuart Barclay
+ */
+api.get("/", (req, res) => {
+  res.status(200);
+  res.json({ Response: "Hello from the API" });
+});
+
+/**
+ * @description base get request route. Reroutes the api call to the api server, gets all sources in the db
+ * @author Stuart Barclay
+ */
+
+api.get("/sources", (req, res, next) => {
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources",
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.end();
+});
+
+/**
+ * @description base post request route. Reroutes the api call to the api server. adds new source to db.
+ * @author Stuart Barclay
+ */
+
+api.post("/sources", (req, res, next) => {
+  let requestBody = "";
+  try {
+    requestBody = JSON.stringify(req.body);
+  } catch (e) {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  }
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(requestBody),
+      },
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.write(requestBody);
+  request.end();
+});
+
+/**
+ * @description base get request route. Reroutes the api call to the api server, gets all sources in the db
+ * @author Stuart Barclay
+ */
+
+api.get("/sources/:sourceId", (req, res, next) => {
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources/" + req.params.sourceId,
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+/**
+ * @description base post request route. Reroutes the api call to the api server. adds new source to db.
+ * @author Stuart Barclay
+ */
+
+api.put("/sources/:sourceId", (req, res, next) => {
+  let requestBody = "";
+  try {
+    requestBody = JSON.stringify(req.body);
+  } catch (e) {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  }
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources/" + req.params.sourceId,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(requestBody),
+      },
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.write(requestBody);
+  request.end();
+});
+
+/**
+ * @description API call to delete a source with the given id.
+ * @author Stuart Barclay
+ */
+
+api.delete("/sources/:sourceId", (req, res, next) => {
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/sources/" + req.params.sourceId,
+      method: "DELETE",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+/**
+ * @description base get request route. Reroutes the api call to the api server, gets all facts in the db
+ * @author Stuart Barclay
+ */
+
+api.get("/facts", (req, res, next) => {
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/facts",
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.end();
+});
+
+/**
+ * @description base post request route. Reroutes the api call to the api server. adds new fact to db.
+ * @author Stuart Barclay
+ */
+
+api.post("/facts", (req, res, next) => {
+  let requestBody = "";
+  try {
+    requestBody = JSON.stringify(req.body);
+  } catch (e) {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  }
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/facts",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(requestBody),
+      },
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+  request.write(requestBody);
+  request.end();
+});
+
+/**
+ * @description base get request route. Reroutes the api call to the api server, gets specific fact from the db
+ * @author Stuart Barclay
+ */
+
+api.get("/facts/:factId", (req, res, next) => {
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/facts/" + req.params.factId,
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+/**
+ * @description API call to delete a source with the given id.
+ * @author Stuart Barclay
+ */
+
+api.delete("/facts/:factId", (req, res, next) => {
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/facts/" + req.params.factId,
+      method: "DELETE",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+api.get("/moderators", (req, res, next) => {
+  /** Validate the user token from header before -> if can't res.status(403).json({"message": "You are not authorised to view this content."}), then check moderator level */
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/moderators",
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+api.post("/moderators", (req, res, next) => {
+  /** Validate the user token from header before -> if can't res.status(403).json({"message": "You are not authorised to view this content."}), then check moderator level */
+  let requestBody = "";
+  try {
+    requestBody = JSON.stringify(req.body);
+  } catch (e) {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  }
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/moderators",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(requestBody),
+      },
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.write(requestBody);
+  request.end();
+});
+
+api.get("/moderators/:emailAddress", (req, res, next) => {
+  /** Validate the user token from header before -> if can't res.status(403).json({"message": "You are not authorised to view this content."}), then check moderator level */
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/moderators/" + req.params.emailAddress,
+      method: "GET",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+api.put("/moderators/:emailAddress", (req, res, next) => {
+  /** Validate the user token from header before -> if can't res.status(403).json({"message": "You are not authorised to view this content."}), then check moderator level */
+  let requestBody = "";
+  try {
+    requestBody = JSON.stringify(req.body);
+  } catch (e) {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  }
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/moderators/" + req.params.emailAddress,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(requestBody),
+      },
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.write(requestBody);
+  request.end();
+});
+
+api.delete("/moderators/:emailAddress", (req, res, next) => {
+  /** Validate the user token from header before -> if can't res.status(403).json({"message": "You are not authorised to view this content."}), then check moderator level */
+  const request = http.request(
+    {
+      host: "localhost",
+      port: 3000,
+      path: "/moderators/" + req.params.emailAddress,
+      method: "DELETE",
+    },
+    (response) => {
+      response.setEncoding("utf-8");
+      let responseString = "";
+      response.on("data", (chunk) => {
+        responseString += chunk;
+      });
+      response.on("end", () => {
+        res.status(response.statusCode).json(JSON.parse(responseString));
+      });
+    }
+  );
+  request.on("error", (e) => {
+    let error = new Error(e.message);
+    error.status = 500;
+    next(error);
+  });
+
+  request.end();
+});
+
+/**
+ * @description API calls done incorrectly will arrive at this route and be dismissed.
+ * @author Stuart Barclay
+ */
+
+api.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+//handles all other errors
+api.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = api;
---
+++ node_modules/db_server/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const sourceRoutes = require("./api/routes/sources");
+const factRoutes = require("./api/routes/facts");
+const moderatorRoutes = require("./api/routes/moderators");
+
+mongoose.connect(
+  // 'mongodb+srv://FakeNewsAdmin:' + process.env.MONGO_ATLAS_PW + '@fake-news-detector-vastj.mongodb.net/fake_news_detector?retryWrites=true&w=majority',
+  "mongodb+srv://FakeNewsAdmin:murrIq-xytbud-2wubjo@fake-news-detector-vastj.mongodb.net/fake_news_detector?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//prevent CORS errors
+app.use((res, req, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Routes which should handle requests
+app.use("/sources", sourceRoutes);
+app.use("/facts", factRoutes);
+app.use("/moderators", moderatorRoutes);
+
+//Requests that passed the routes above are not supported and therefore seen as errors.
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+//handles all other errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
