85378d2ad0f3c5d3a4455b7f35a3238fd8c619dc
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-07-12 00:06:45 +0200

    removed bcrypt and added bcryptjs

--- db_server/api/routes/moderators.js
+++ db_server/api/routes/moderators.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const Moderator = require("../models/moderator");
 
