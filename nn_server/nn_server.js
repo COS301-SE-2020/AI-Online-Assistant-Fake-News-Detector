15c96f68c019aa495b5b9e00b27fc46442c7470b
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-07-11 04:41:04 +0200

    Updated nn port, updated package.json to include the nn server to be run from api_server

--- nn_server/nn_server.js
+++ nn_server/nn_server.js
@@ -5,16 +5,16 @@ const sentenceRegex = /[^\.\!\?]*[\.\!\?]/g;
 const precision = 8;
 
 // Port to listen on
-const port = 8080;
+const port = 8082;
 
 // Neural network, procesing modules
-const core_nn = require('./core_nn/core_nn.js');
-const nn_spelling_input = require('./nn_spelling_input/nn_spelling_input.js');
-const nn_verbose_input = require('./nn_verbose_input/nn_verbose_input.js');
+const core_nn = require("./core_nn/core_nn.js");
+const nn_spelling_input = require("./nn_spelling_input/nn_spelling_input.js");
+const nn_verbose_input = require("./nn_verbose_input/nn_verbose_input.js");
 
 // HTTP modules
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const server = express();
 
@@ -26,29 +26,33 @@ core.addInput(new nn_spelling_input());
 core.addInput(new nn_verbose_input());
 
 // configure express to use body-parser as middle-ware
-server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 // API implementation
-server.post('/API/Check', (req, res) => {  
-    if (req.body.hasOwnProperty('type') &&
-	req.body.type === 'text' &&
-	req.body.hasOwnProperty('content') &&
-	typeof req.body.content === 'string')
-    {
-	let sentences = req.body.content.match(sentenceRegex); // splits text into array of sentences
-	let result = core.processText(sentences);
-	if (!isNaN(parseFloat(result))) { // check if result is float to avoid returing internal errors to clients
-	    res.status(200).json({result: parseFloat(result.toFixed(precision))});
-	} else {
-	    console.log(result);
-	    res.sendStatus(500);
-	}	    
+server.post("/API/Check", (req, res) => {
+  if (
+    req.body.hasOwnProperty("type") &&
+    req.body.type === "text" &&
+    req.body.hasOwnProperty("content") &&
+    typeof req.body.content === "string"
+  ) {
+    let sentences = req.body.content.match(sentenceRegex); // splits text into array of sentences
+    let result = core.processText(sentences);
+    if (!isNaN(parseFloat(result))) {
+      // check if result is float to avoid returing internal errors to clients
+      res.status(200).json({ result: parseFloat(result.toFixed(precision)) });
     } else {
-	res.sendStatus(400);
+      console.log(result);
+      res.sendStatus(500);
     }
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-server.listen(port, () => {console.log("nn_server listening at http://localhost:" + port)});
+server.listen(port, () => {
+  console.log("NN_Server listening on port " + port);
+});
 
 module.exports = server;
