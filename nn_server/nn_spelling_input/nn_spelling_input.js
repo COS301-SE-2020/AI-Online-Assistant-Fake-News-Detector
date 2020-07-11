6c361c345014afbd2b768cbd673125d0b4b0b52c
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-07-11 11:44:31 +0200

    Added path to the nn_server, changed path routes

--- nn_server/nn_spelling_input/nn_spelling_input.js
+++ nn_server/nn_spelling_input/nn_spelling_input.js
@@ -1,5 +1,7 @@
-const nn_input = require('../nn_input/nn_input.js');
-const spellChecker = require('simple-spellchecker');
+const root = require("./Util/path");
+const path = require("path");
+const nn_input = require(path.join(root, "nn_input", "nn_input.js"));
+const spellChecker = require("simple-spellchecker");
 
 const wordRegex = /\b[^\s]+\b/g;
 
@@ -11,31 +13,32 @@ const dictionaryGB = spellChecker.getDictionarySync("en-GB"); // British
  * @description a class which determines the fraction of english spelling errors.
  * @author Alistair Payn
  */
-class nn_spelling_input extends nn_input {    
-    constructor(precision) {
-	super(precision);
-    }
+class nn_spelling_input extends nn_input {
+  constructor(precision) {
+    super(precision);
+  }
 
-    /**
-     * @description determines the fraction of english spelling errors.
-     * @author Alistair Payn
-     */
-    process(sentences) {
-	let spellingErrorCount = 0;
-	let wordCount = 0;
-	sentences.forEach((sentence) => {
-	    let words = sentence.match(wordRegex);
-	    words.forEach((word) => {
-		++wordCount;
-		spellingErrorCount += (!dictionaryUS.spellCheck(word) && !dictionaryGB.spellCheck(word));
-	    });
-	});
-	let result = 0.0;
-	if (wordCount > 0) {
-	    result = spellingErrorCount / wordCount;
-	} 
-	return result;
+  /**
+   * @description determines the fraction of english spelling errors.
+   * @author Alistair Payn
+   */
+  process(sentences) {
+    let spellingErrorCount = 0;
+    let wordCount = 0;
+    sentences.forEach((sentence) => {
+      let words = sentence.match(wordRegex);
+      words.forEach((word) => {
+        ++wordCount;
+        spellingErrorCount +=
+          !dictionaryUS.spellCheck(word) && !dictionaryGB.spellCheck(word);
+      });
+    });
+    let result = 0.0;
+    if (wordCount > 0) {
+      result = spellingErrorCount / wordCount;
     }
+    return result;
+  }
 }
 
 module.exports = nn_spelling_input;
--- nn_server/node_modules/core_nn/core_nn.js
+++ nn_server/node_modules/core_nn/core_nn.js
@@ -1,49 +1,51 @@
-const nn_input = require('../nn_input/nn_input.js');
+const root = require("./Util/path");
+const path = require("path");
+const nn_input = require(path.join(root, "nn_input", "nn_input.js"));
 
 class core_nn extends nn_input {
-    constructor(trainedModel) {
-	super();
-	this.model = trainedModel;
-	this.inputs = [];
-    }
+  constructor(trainedModel) {
+    super();
+    this.model = trainedModel;
+    this.inputs = [];
+  }
 
-    importModel(trainedModel) {
-	this.model = trainedModel;
-    }
+  importModel(trainedModel) {
+    this.model = trainedModel;
+  }
 
-    exportModel() {
-	return this.model;
-    }
+  exportModel() {
+    return this.model;
+  }
 
-    runModel(inputResults) {
-	let result = 0.0;
-	if (inputResults.length > 0) {
-	    let total = 0.0;
-	    inputResults.forEach((value)=>{
-		total += value;
-	    });
-	    result = total;
-	}
-	return result;
+  runModel(inputResults) {
+    let result = 0.0;
+    if (inputResults.length > 0) {
+      let total = 0.0;
+      inputResults.forEach((value) => {
+        total += value;
+      });
+      result = total;
     }
-    
-    processText(sentences) {
-	let inputResults = [];
-	let result = 0.0;
+    return result;
+  }
 
-	// get the result of each input and then get the result from the regression model
-	this.inputs.forEach((input) => {
-	    inputResults.push(input.process(sentences));
-	    if (inputResults.length === this.inputs.length) {
-		result = this.runModel(inputResults);
-	    }
-	});
-	return result;
-    }
+  processText(sentences) {
+    let inputResults = [];
+    let result = 0.0;
 
-    addInput(input) {
-	this.inputs.push(input);
-    }
+    // get the result of each input and then get the result from the regression model
+    this.inputs.forEach((input) => {
+      inputResults.push(input.process(sentences));
+      if (inputResults.length === this.inputs.length) {
+        result = this.runModel(inputResults);
+      }
+    });
+    return result;
+  }
+
+  addInput(input) {
+    this.inputs.push(input);
+  }
 }
 
 module.exports = core_nn;
