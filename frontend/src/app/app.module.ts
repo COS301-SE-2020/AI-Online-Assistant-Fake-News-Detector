519810d976d6d817fd156c1d79861ce2e888463e
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-07-15 04:20:45 +0200

    changed the fronend to frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NavComponent } from './nav/nav.component'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { InputComponent } from './input/input.component'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input'
+import { ReportService } from './report.service'
+import { FindService } from './find.service'
+import { CheckService } from './check.service'
+import { MatCardModule } from '@angular/material/card'
+
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ModerateComponent } from './components/moderate/moderate.component';
+import { HowtoComponent } from './components/howto/howto.component'
+
+@NgModule({
+	declarations: [ AppComponent, NavComponent, InputComponent, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, ModerateComponent, HowtoComponent ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		LayoutModule,
+		MatToolbarModule,
+		MatButtonModule,
+		MatSidenavModule,
+		MatIconModule,
+		MatListModule,
+		MatFormFieldModule,
+		FormsModule,
+		MatInputModule,
+		HttpClientModule,
+		MatCardModule
+	],
+	providers: [ ReportService, FindService, CheckService, HttpClientModule ],
+	bootstrap: [ AppComponent ]
+})
+export class AppModule {}
---
+++ frontend/src/app/check.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs'
+import { catchError, retry } from 'rxjs/operators'
+
+@Injectable({
+	providedIn: 'root'
+})
+export class CheckService {
+	constructor(private http: HttpClient) {}
+
+	check(content) {
+		let body = {
+			type: 'source',
+			content: content
+		}
+		return this.http.get<any>('http://localhost:8080/api/sources/' + content, { observe: 'response' })
+	}
+}
