57639df837915cab54d24a132c69a6a32cc6b31d
Stuey61296 <53872200+Stuey61296@users.noreply.github.com>
2020-07-15 12:50:45 +0200

    updated swagger path

--- api_server/api_server.js
+++ api_server/api_server.js
@@ -5,7 +5,7 @@ const port = 8080;
 const helmet = require("helmet");
 const morgan = require("morgan");
 var cors = require("cors");
-const pathToSwaggerUi = require("swagger-ui-dist").getAbsoluteFSPath();
+const SwaggerUi = require("./routes/swagger-ui-dist").getAbsoluteFSPath();
 const API = require("./routes/APIv1");
 
 //Static files like css or js frontend.
@@ -16,7 +16,7 @@ server.use(bodyParser.json({ type: "application/json" }));
 server.use(helmet());
 server.use(cors());
 server.use(morgan("combined"));
-server.use("/API-Documents", express.static(pathToSwaggerUi));
+server.use("/API-Documents", express.static(SwaggerUi));
 server.use("/API", API);
 server.use("/", express.static("../frontend/dist/AiNews"));
 
