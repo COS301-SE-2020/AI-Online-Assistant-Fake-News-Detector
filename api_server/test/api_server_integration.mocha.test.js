bf14ff16cb30c452a2abc28c42c84e1b658feb02
Quinton <qcoetzee771@gmail.com>
2020-07-31 15:09:53 +0200

    Separated Production from Development for testing

--- api_server/test/api_server_integration.mocha.test.js
+++ api_server/test/api_server_integration.mocha.test.js
@@ -6,6 +6,25 @@ const server = require("../api_server");
 
 chai.use(chaiHttp);
 describe("API-DB Integration, Get and Update", () => {
+  it("It should add a single source to the database /POST sources", function (done) {
+    chai
+      .request(server)
+      .post("/API/sources")
+      .send({ name: "gmail", rating: 5, tld: "https://gmail.com/" })
+      .end(function (err, res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a("object");
+        res.body.response.should.have.property("message");
+        res.body.response.Source.should.be.a("object");
+        res.body.response.Source.should.have.property("Name");
+        res.body.response.Source.should.have.property("Domain Name");
+        res.body.response.Source.should.have.property("ID");
+        res.body.response.Source.should.have.property("Rating");
+        res.body.response.Source.Rating.should.equal(5);
+        done();
+      });
+  });
   it("It should get a full list of available sources", (done) => {
     chai
       .request(server)
@@ -27,6 +46,7 @@ describe("API-DB Integration, Get and Update", () => {
 
   it("It should get the top available source based on name and update its rating", (done) => {
     const source = {
+      // rating: 90000,
       rating: (Math.random() * 100).toFixed(0),
     };
     chai
@@ -53,6 +73,31 @@ describe("API-DB Integration, Get and Update", () => {
                 );
                 done();
               });
+          });
+      });
+  });
+  it("It should delete a single source from the database /DELETE source/:source", function (done) {
+    chai
+      .request(server)
+      .get("/API/Sources")
+      .end((err, responder) => {
+        chai
+          .request(server)
+          .delete(
+            "/API/Sources/id/" +
+              responder.body.response.Sources[
+                responder.body.response.Sources.length - 1
+              ].ID
+          )
+          .end(function (error, res) {
+            console.log(res.body);
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a("object");
+            res.body.response.should.have.property("message");
+            res.body.response.message.should.be.a("string");
+            res.body.response.message.should.be.eql("Source deleted");
+            done();
           });
       });
   });
