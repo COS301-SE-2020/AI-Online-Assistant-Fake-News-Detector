b5b835ac148e5c66ed14e7be75324c45354e50c3
Taydos <u18010408@tuks.co.za>
2020-08-15 08:19:15 +0200

    Updated JSON Selectors For Services
    To accomodate the changes made to the API route paths, I modified the selectors responsible for accessing the relevant JSON object values from HTTP requests.
    
    Other changes include:
    -Changed the application name on the navigation bar and browser title bar.
    -Removed the white background of the group photo in the About page.
    -Changed sign-in form colors to prevent color blending.
    -Changed footer color and modified footer content.

--- frontend/src/app/delete-source.service.ts
+++ frontend/src/app/delete-source.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DeleteSourceService {
 
-  url = 'http://fakenewsdetector.tech/api/sources/';
+  url = 'http://fakenewsdetector.tech/api/sources/id/';
 
   constructor(private _http: HttpClient) { }
 
