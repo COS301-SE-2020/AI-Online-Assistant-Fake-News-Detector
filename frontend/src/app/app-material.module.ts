59e6dc95962e8a315062f15dc1f25784480414a6
Danré Retief <iamdanre@gmail.com>
2020-08-24 23:31:48 +0200

    updated home page

--- frontend/src/app/app-material.module.ts
+++ frontend/src/app/app-material.module.ts
@@ -4,8 +4,19 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-	exports: [ MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, MatBottomSheetModule ]
+  exports: [ MatButtonModule,
+            MatIconModule,
+            MatMenuModule,
+            MatToolbarModule,
+            MatBottomSheetModule,
+            MatTabsModule,
+            MatInputModule,
+            MatDividerModule,
+          ]
 })
 export class AiMaterialModule {}
