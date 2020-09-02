b82e7990d838647a195b7e8f70e5c38682cc5dbb
Danré Retief <iamdanre@gmail.com>
2020-09-02 17:32:11 +0200

    updates, auth and organise
    mostly plumbing. organised layout. header is no longer root so website scales beautifuly, should be quicker as well. moderator is hidden if you're not signed in. auth flakes without httpS. auth_config not legit the real keys are on Auth0.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,49 +1,58 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-//deprecated
-//import { NavComponent } from './nav/nav.component'
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ModerateComponent } from './components/moderate/moderate.component';
-import { HowtoComponent } from './components/howto/howto.component';
-import { NotfoundComponent } from './notfound/notfound.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+// deprecated
+// import { NavComponent } from './nav/nav.component'
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ModerateComponent } from "./components/moderate/moderate.component";
+import { HowtoComponent } from "./components/howto/howto.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { AuthGuard } from "./auth/auth.guard";
+import { LoginComponent } from "../app/components/login/login.component";
 
 const routes: Routes = [
-	{
-		path: '',
-		component: HomeComponent,
-		pathMatch: 'full'
-	},
-	{
-		path: 'home',
-		//possibly redirect here instead??
-		//redirectTo: ''
-		component: HomeComponent
-	},
-	{
-		path: 'about',
-		component: AboutComponent,
-		data: { state: 'about' }
-	},
-	{
-		path: 'moderate',
-		component: ModerateComponent,
-		data: { state: 'moderate' }
-	},
-	{
-		path: 'howto',
-		component: HowtoComponent,
-		data: { state: 'howto' }
-	},
-	{
-		path: '**',
-		component: NotfoundComponent,
-		data: { state: 'notfound' }
-	}
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    // possibly redirect here instead??
+    // redirectTo: ''
+    component: HomeComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+    data: { state: "about" },
+  },
+  {
+    path: "moderate",
+    component: ModerateComponent,
+    canActivate: [AuthGuard],
+    data: { state: "moderate" },
+  },
+  {
+    path: "howto",
+    component: HowtoComponent,
+    data: { state: "howto" },
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    canActivate: [AuthGuard],
+    data: { state: "notfound" },
+  },
+  {
+    path: "**",
+    component: NotfoundComponent,
+    data: { state: "notfound" },
+  },
 ];
 
 @NgModule({
-	imports: [ RouterModule.forRoot(routes) ],
-	exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ frontend/src/app/share/sharesheet.component.ts
@@ -0,0 +1,67 @@
+import { Component } from "@angular/core";
+import { MatBottomSheetRef } from "@angular/material/bottom-sheet";
+
+@Component({
+  selector: "app-sharesheet",
+  templateUrl: "./sharesheet.component.html",
+})
+export class ShareSheetComponent {
+  cantshare: boolean;
+  constructor(private _bottomSheetRef: MatBottomSheetRef<ShareSheetComponent>) {
+    // if (!navigator.share) {
+    // 	console.log('Web Share API is not available in your browser.');
+    // 	//this.cantshare = true;
+    // } else {
+    // 	this.cantshare = false;
+    // }
+  }
+
+  openLink(event: MouseEvent, link: string): void {
+    this._bottomSheetRef.dismiss();
+    // enabling this will open links in a new tab
+    event.preventDefault();
+    // below does not open new tab
+    // window.location.href = link;
+    // this does new tab too if mouse event is ignored
+    window.open(link);
+  }
+
+  mail(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+    window.open("mailto:5bits301@gmail.com?subject=Artifact [x__O]");
+  }
+
+  share(event: MouseEvent) {
+    event.preventDefault();
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "ArtiFact",
+          url: "https://fakenewsdetector.tech",
+        })
+        .then(() => {
+          console.log("Successful share");
+          this._bottomSheetRef.dismiss();
+        })
+        .catch((error) => console.log("Error sharing", error));
+    } else {
+      document.getElementById("other").innerHTML =
+        "Web Share API not supported";
+      this.cantshare = true;
+    }
+  }
+
+  async copy(event: MouseEvent) {
+    try {
+      await navigator.clipboard.writeText("https://ArtiFact.me");
+      console.log("Page URL copied to clipboard");
+      document.getElementById("copy").innerHTML = "Link copied to clipboard";
+      document.getElementById("copyicon").innerHTML = "tag_faces";
+    } catch (err) {
+      console.error("Copy to clipboard failed: ", err);
+      document.getElementById("copy").innerHTML = "Failed to copy to clipboard";
+      document.getElementById("copyicon").innerHTML = "report";
+    }
+  }
+}
