9c243a6e5fd67d1e5f719344a1c035799bcee6fb
AlistairPaynUP <u14272289@tuks.co.za>
2020-09-10 14:32:46 +0200

    Added postprocessing.py, labels.py, updated api_methods.py. Added breakdown of text article predictions (per sample input into nn), api_methods for storing and retrieving models, training data (will need to update when new authentication implemented).

---
+++ nn_server/neural_network_utilities/labels.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class RealOrFakeLabels:
+    @staticmethod
+    def outputToLabel(output):
+        maxIndex = np.argmax(output)
+        if maxIndex == 0:
+            return 'real'
+        else:
+            return 'fake'
+
+    @staticmethod
+    def outputToValue(output):
+        return output[np.argmax(output)]
+
+    @staticmethod
+    def labelToOutput(label):
+        if label == 'real':
+            return [1, 0]
+        else:  # 'fake'
+            return [0, 1]
