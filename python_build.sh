dac110f7b4a9e464f1ef47c2c5822619115d73c5
AlistairPaynUP <u14272289@tuks.co.za>
2020-09-11 12:29:21 +0200

    Updated build scripts.

--- python_build.sh
+++ python_build.sh
@@ -1,3 +1,2 @@
 pip3 install -r requirements.txt > log.txt
 python -m spacy download en_core_web_lg > log.txt
-npm install
--- python_tests.sh
+++
@@ -1,1 +0,0 @@
-for test in `find $directory -type f -name "*.test.py"`; do  python $test; done
